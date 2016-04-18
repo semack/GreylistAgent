699d121d8dec4a3d7f01c0ec0fdd0c6795ad83b7
Andriy S'omak <semack@gmail.com>
2016-04-18 20:21:31 +0300

    amend

--- src/GreyListAgent.Configurator/Program.cs
+++ src/GreyListAgent.Configurator/Program.cs
@@ -1,8 +1,6 @@
 ﻿using GreyListAgent.Configurator.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GreyListAgent.Configurator
